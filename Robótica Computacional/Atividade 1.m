6eac710834c31f7481f7468a8391e8571beb2990
Victor Brito Tavares <victorbritotavares13@gmail.com>
2025-09-09 17:53:40 +0000

    .

---
+++ Robótica Computacional/Atividade 1.m
@@ -0,0 +1,59 @@
+clear % remove todas as váriaveis
+clc % limpa a janela de comandos
+close all % fecha todas as figuras gráficas abertas
+
+## Pacote simbólico % biblioteca com variáveis
+pkg load symbolic % carregar as funções do pacote
+
+syms th dx dy dz L1 l2 L3 L4 L5 L6 L7 real % declarar variáveis simbólicas
+syms th1 th2 th3 th4 th5 th6 th7 real %declarar variáveis simbólicas
+
+one = sym(1); zero = sym(0); % declaração de duas variáveis simbólicenseas (0 & 1)
+deg2rad_sym = @(d)sym(d) * sym(pi)/ sym(180); %usada para converter ângulos expressos em graus para radiano
+
+T = [ zero zero zero one;% condição de existência
+      one zero zero dx; % X1, Y1 , Z1 relacionadas ao X0 / Criação da matriz de translação 4x4
+      zero one zero dy;% X1, Y1 , Z1 relacionadas ao Y0
+      zero zero one dz];% X1, Y1 , Z1 relacionadas ao Z0
+      
+Rx = [ one   zero     zero     zero; % Eixo X1,onde o X1 vai ser rotacionado e comparado com X0,Y0 & Z0
+       zero  cos(th) -sin(th)  zero; % Eixo Y1,onde o X1 vai ser rotacionado e comparado com X0,Y0 & Z0
+       zero  sin(th)  cos(th)  zero; % Eixo Z1,onde o X1 vai ser rotacionado e comparado com X0,Y0 & Z0
+       zero  zero     zero     one]; %Linha para montar matriz generalizada
+       
+       
+ Rz= [ cos(th) -sin(th) zero zero;  %Eixo X, onde o Z1 vai ser rotacionado e comparado X0,Y0 & Z0
+       sin(th) cos(th) zero zero; %Eixo Y1,onde o Z1 vai ser rotacionado e comparado com X0,Y0 & Z0
+       zero     zero     one  zero; %Eixo Z1,onde o Z1 vai ser rotacionado e comparado com X0,Y0 & Z0
+       zero     zero     one  zero]; % Linha para montar matriz generalizada
+       
+ F0=sym(eye(4)); % criação de matriz identidade 4x4 e conversão para símbolos matemáticos
+ 
+ Tr_0_1 = subs(T, [dx dy dz], [zero zero zero]); % inicializar o sistema homogêneo
+ TH_0_1 = Tr_0_1 * subs(Rz, th, th1); % comando de movimentação sendo o antigo * o novo para movimentação
+ F1 = F0 * TH_0_1;% criação do frame inercial
+ 
+ pi2 = sym(pi)/2; % declarando pi2 / pi2=3,14/2, como valor simbólico
+ 
+ Tr_1_2 = subs(T, [dx dy dz], [zero zero L1]) * subs(Rx, th, pi2;
+ TH_1_2 = Tr_1_2 * subs(Rz, th, th2)
+ F2 = F1 * TH_1_2;
+ 
+ Tr_2_3 = subs(T, [dx dy dz], [zero L2 zero]) * subs(Rx, th, -pi2);
+ TH_2_3 = Tr_2_3 * subs(Rz, th, th3);
+ F3 = F2 * TH_2_3;
+ 
+ Tr_3_4 = subs(T, [dx dy dz], [zero zero L3]) * subs(Rx, th, pi2);
+ TH_3_4 = Tr_3_4 * subs(Rz, th, th4);
+ F4 = F3 * TH_3_4;
+
+ Tr_4_5 = subs(T, [dx dy dz], [zero L4 zero]) * subs(Rx, th, -pi2);
+ TH_4_5 = Tr_4_5 * subs(Rz, th, th5);
+ F3 = F2 * TH_2_3; 
+       
+
+       
+       
+
+
+
